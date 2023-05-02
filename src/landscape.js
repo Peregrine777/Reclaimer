2c892f0ac3c2b071d329b312ad76e4902cce94e3
Samuel McKenzie-Sell <samuel.m.mckenzie-sell@student.uts.edu.au>
2023-05-02 18:02:42 +1000

    separate size and city radius

--- src/landscape.js
+++ src/landscape.js
@@ -1,11 +1,14 @@
 import * as THREE from 'three';
 import { ImprovedNoise } from '../build/math/ImprovedNoise.js';
+import { randFloat, randInt } from '../src/MathUtils.js';
 
 export class Landscape {
   size = 0;
+  cityRadius = 0;
 
   constructor(size) {
-    this.size = size;
+    this.cityRadius = size * 0.5;
+    this.size = size * 10;
   }
 
   // Path: lanscape.js
@@ -30,9 +33,9 @@ export class Landscape {
         let positionAttribute = landGeom.attributes.position;
         Land.name = "Land";
 
-        this.addNoise(Land, 0.25, 5, 0.15);
+        this.addNoise(Land, 0.25, 5, 0.10);
         this.addNoise(Land, 0.50, 5, 0.20);
-        this.addNoise(Land, 1.00, 10, 0.10);
+        this.addNoise(Land, 5, 5, 0.2);
 
 
         return Land;
@@ -43,17 +46,19 @@ addNoise(object, scaleX, scaleY, height){
   let geometry = object.geometry
   let positionAttribute = geometry.attributes.position;
   let n = new ImprovedNoise;
+  let rand = randFloat(0, 100);
     
   for (let i = 0 ; i < positionAttribute.count ; i++) {
     //console.log(positionAttribute.getX(i));
     const x = positionAttribute.getX(i);
     const y = positionAttribute.getY(i);
     const z = positionAttribute.getZ(i);
+   
 
     let dist = new THREE.Vector2(x, y).distanceTo(new THREE.Vector2(0,0))
-    if (dist > this.size/2){
-      let h = n.noise(x * scaleX,y * scaleY, 1);
-      h *= height
+    if (dist > this.cityRadius){
+      let h = n.noise(x * scaleX,y * scaleY, rand);
+      h *= height*(dist/this.cityRadius);
       //let z = positionAttribute.getZ(i);
       positionAttribute.setZ(i, z+h);
     }
