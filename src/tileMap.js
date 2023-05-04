1f1689c2e240dfa7d13d098d4e1c570f20b7c82e
Samuel McKenzie-Sell <samuel.m.mckenzie-sell@student.uts.edu.au>
2023-05-04 12:21:55 +1000

    init tileMap

---
+++ src/tileMap.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+import { ImprovedNoise } from '../build/math/ImprovedNoise.js';
+import { randFloat, randInt, smoothstep } from '../src/MathUtils.js';
+
+export class Landscape {
+
+}
