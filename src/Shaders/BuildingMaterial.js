94c06b0942c6bd023ad56265a493b70d640976b9
Samuel McKenzie-Sell <samuel.m.mckenzie-sell@student.uts.edu.au>
2023-06-01 13:58:02 +1000

    Better degredation

--- src/Shaders/BuildingMaterial.js
+++ src/Shaders/BuildingMaterial.js
@@ -114,29 +114,97 @@ export const BuildingShader = {
         return mix(x1, x2, xy.y);
     }
 
-    //Meant to correct normal map given surface normals... doesn't work.
-    // vec3 perturbNormal2Arb( vec3 eye_pos, vec3 surf_norm, vec3 mapN ) {
-
-	// 	// Workaround for Adreno 3XX dFd*( vec3 ) bug. See #9988
-
-	// 	vec3 q0 = vec3( dFdx( eye_pos.x ), dFdx( eye_pos.y ), dFdx( eye_pos.z ) );
-	// 	vec3 q1 = vec3( dFdy( eye_pos.x ), dFdy( eye_pos.y ), dFdy( eye_pos.z ) );
-	// 	vec2 st0 = dFdx( vUv.st );
-	// 	vec2 st1 = dFdy( vUv.st );
-
-	// 	float scale = sign( st1.t * st0.s - st0.t * st1.s ); // we do not care about the magnitude
-
-	// 	vec3 S = normalize( ( q0 * st1.t - q1 * st0.t ) * scale );
-	// 	vec3 T = normalize( ( - q0 * st1.s + q1 * st0.s ) * scale );
-	// 	vec3 N = normalize( surf_norm );
-
-	// 	mat3 tsn = mat3( S, T, N );
-
-	// 	mapN.xy *= ( float( gl_FrontFacing ) * 2.0 - 1.0 );
-
-	// 	return normalize( tsn * mapN );
-
-	// }
+    vec3 mod289(vec3 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+      
+      vec4 mod289(vec4 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+      
+      vec4 permute(vec4 x) {
+           return mod289(((x*34.0)+1.0)*x);
+      }
+      
+      vec4 taylorInvSqrt(vec4 r)
+      {
+        return 1.79284291400159 - 0.85373472095314 * r;
+      }
+      
+      float snoise(vec3 v)
+        {
+        const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+        const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+      
+      // First corner
+        vec3 i  = floor(v + dot(v, C.yyy) );
+        vec3 x0 =   v - i + dot(i, C.xxx) ;
+      
+      // Other corners
+        vec3 g = step(x0.yzx, x0.xyz);
+        vec3 l = 1.0 - g;
+        vec3 i1 = min( g.xyz, l.zxy );
+        vec3 i2 = max( g.xyz, l.zxy );
+      
+        //   x0 = x0 - 0.0 + 0.0 * C.xxx;
+        //   x1 = x0 - i1  + 1.0 * C.xxx;
+        //   x2 = x0 - i2  + 2.0 * C.xxx;
+        //   x3 = x0 - 1.0 + 3.0 * C.xxx;
+        vec3 x1 = x0 - i1 + C.xxx;
+        vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
+        vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
+      
+      // Permutations
+        i = mod289(i);
+        vec4 p = permute( permute( permute(
+                   i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+                 + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
+                 + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+      
+      // Gradients: 7x7 points over a square, mapped onto an octahedron.
+      // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+        float n_ = 0.142857142857; // 1.0/7.0
+        vec3  ns = n_ * D.wyz - D.xzx;
+      
+        vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
+      
+        vec4 x_ = floor(j * ns.z);
+        vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+      
+        vec4 x = x_ *ns.x + ns.yyyy;
+        vec4 y = y_ *ns.x + ns.yyyy;
+        vec4 h = 1.0 - abs(x) - abs(y);
+      
+        vec4 b0 = vec4( x.xy, y.xy );
+        vec4 b1 = vec4( x.zw, y.zw );
+      
+        //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
+        //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
+        vec4 s0 = floor(b0)*2.0 + 1.0;
+        vec4 s1 = floor(b1)*2.0 + 1.0;
+        vec4 sh = -step(h, vec4(0.0));
+      
+        vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+        vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+      
+        vec3 p0 = vec3(a0.xy,h.x);
+        vec3 p1 = vec3(a0.zw,h.y);
+        vec3 p2 = vec3(a1.xy,h.z);
+        vec3 p3 = vec3(a1.zw,h.w);
+      
+      //Normalise gradients
+        vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+        p0 *= norm.x;
+        p1 *= norm.y;
+        p2 *= norm.z;
+        p3 *= norm.w;
+      
+      // Mix final noise value
+        vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+        m = m * m;
+        return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
+                                      dot(p2,x2), dot(p3,x3) ) );
+        }
 
 
     void main() {
@@ -148,11 +216,12 @@ export const BuildingShader = {
         vec3 fogColor = vec3(0.6, 0.62, 0.85);
         vec3 apartmentWindowColor = vec3(0.2, 0.2, 0.5);
         vec3 windowColor = vec3(0.7, 0.7, 0.4);
+        vec3 mud1  = vec3(0.3, 0.3, 0.35);
+        vec3 mud2 = vec3(0.06, 0.06, 0.05);
 
-        vec3 shadowColor = vec3(0, 0, 0);
-        float shadowPower = 0.5;
 
-        hmin = clamp(hmin + frame,-10.,10.);
+
+        hmin = clamp(hmin + frame,-10.,7.);
         hmax = clamp(hmax + frame,0.0,12.);
         
 
@@ -163,6 +232,8 @@ export const BuildingShader = {
         // vec3 base = baseColor + texture2D(textureMap, vUv).rgb;
         vec3 base = baseColor * hValue;
         float roof = (1. - step(0.3,vModel.g));
+
+        vec3 mud = mix(mud1, mud2, snoise(vPosition*3.));
         
         
         if (type == 1){
@@ -188,13 +259,18 @@ export const BuildingShader = {
             vec2 fpos = fract(st);
             vec3 winMos = vec3(vec3(rand( ipos2 )));
 
+            float window = 0.0;
+
             vec3 mosaicCol = vec3(mod(ipos.x + ipos.y,2.0));
-            float window = (step(-0.3,vModel.g)- step(0.4,vModel.g)) * mosaicCol.g * winMos.g ;
+            if (frame == 0.){
+                window = (step(-0.3,vModel.g)- step(0.4,vModel.g)) * mosaicCol.g * winMos.g ;
+            }
+            
             base = mix(base, windowColor, window);
             // base = winMos;
         }
 
-        base = mix(base, vec3(0.2,0.2,0.2), (1.-hValue) );
+        base = mix(base, (base * 0.5) + (mud * 0.5), (1.- hValue));
         // vec3 base = vec3(frame, frame, frame);
 
 
@@ -233,7 +309,6 @@ export const BuildingShader = {
 
         float fog = viewZ.z/5000.;
         vec3 c = mix(finalLighting, ambientColor, ambientStrength);
-        c = mix(c, shadowColor, (1.0 - getShadowMask() ) * shadowPower);
 
         vec3 finalFog = mix(c, fogColor, fog);
         gl_FragColor = vec4(finalFog ,  1.0 );
